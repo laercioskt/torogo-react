fe3c9ddf7abed0560676505c13d0fe15ae0c328c
Laercio Pereira <laercio.pereira@objective.com.br>
2017-12-23 19:43:03 -0200

    dynamic board and rectangles on borders

--- src/containers/App/board/Board.js
+++ src/containers/App/board/Board.js
@@ -7,10 +7,13 @@ class Board extends Component {
 
 render() {    
     var svgFragments = [];
-    for (var i = 0; i <= 18; i++) {
-      for (var j = 0; j <= 18; j++) {
+    var length = this.props.pieces.size;
+    for (var i = length * -1; i <= length * 2; i++) {
+      for (var j = length * -1; j <= length * 2; j++) {
         svgFragments.push(<BoardSlot col={i} row={j} actions={this.props.actions} />);
-        var piece = this.props.pieces.getIn([i,j]);
+        var col = i < 0 ? length + i % length : i % length; 
+        var row = j < 0 ? length + j % length : j % length;
+        var piece = this.props.pieces.getIn([row, col]);
         if (piece === 1) svgFragments.push( <BoardPiece col={i} row={j} color="#F9F9F9" />);
         if (piece === 2) svgFragments.push( <BoardPiece col={i} row={j} color="#222222" />);
       }
@@ -20,14 +23,20 @@ render() {
     var y = this.props.boardPosition.getIn("y");
     return (
       <div >
-          <span>{}</span>
           <button onClick={() => this.props.actions.moveBoard({x: -1, y: 0})}>Left</button>
           <button onClick={() => this.props.actions.moveBoard({x: 1, y: 0})}>Right</button>
           <button onClick={() => this.props.actions.moveBoard({x: 0, y: -1})}>Up</button>
           <button onClick={() => this.props.actions.moveBoard({x: 0, y: 1})}>Down</button>
-          <svg height="800" width="100%" viewBox={x + " " + y + " 18 18"} preserveAspectRatio="xMinYMin meet">
-             <rect height="18" width="18" y="0" x="0" fill="#ffbd13"/>
-            {svgFragments}
+          <svg height="600" width="100%" viewBox={"-1 -1 "+(length+2)+" "+ (length+2)} preserveAspectRatio="xMinYMin meet">
+            <g transform={"translate("+x+","+y+")"} >
+              <rect height={length} width={length} y="0" x="0" fill="#ffbd13"/>
+              {svgFragments}
+            </g>
+            <g><rect height="100%" width="100" y="-1" x="-100" fill="#FF0000"/>
+            <rect height="100%" width="100" y="-1" x={length} fill="#FF0000"/>
+            <rect height="100" width="100%" y="-100" x="-1" fill="#FF0000"/>
+            <rect height="100" width="100%" y={length} x="-1" fill="#FF0000"/>
+            </g>
           </svg>
       </div>
     );
--- src/containers/App/reducer.js
+++ src/containers/App/reducer.js
@@ -8,38 +8,41 @@ import {
   GET_API_DATA_ERROR,
 } from './constants';
 
+const matrixGenerator = (size) => {
+  var results = new Array();
+  for (let i = 0; i < size; i++) {
+    var row = new Array();
+    for (let j = 0; j < size; j++)
+      row.push(Math.floor((Math.random() * 3)));
+    results.push(row);
+  }
+  return results;
+}
+
 const initialState = fromJS({
   apiData: null,
   apiDataLoading: true,
   apiDataLoaded: null,
   apiDataError: null,
-  boardPosition: {x: 0.5, y: 0.5},
-  pieces: [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], 
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+  boardPosition: {x: 1, y: 1},
+  pieces: matrixGenerator(8),
 });
 
 const appReducer = (state = initialState, action) => {
   switch (action.type) {
     case MOVE_BOARD:
-      return state.update('boardPosition', (boardPosition) => {
-        var newBoardPosition = boardPosition.update('x', (x) => x + action.data.x);
-        return newBoardPosition.update('y', (y) => y + action.data.y);
+        var size = state.get('pieces').size;
+        return state.update('boardPosition', (boardPosition) => {
+        var newBoardPosition = boardPosition.update('x', (x) => {
+          if (x > (size/2)) x = x - size;
+          if (x < (size/2)*-1) x = x + size;
+          return x + action.data.x;
+        });
+        return newBoardPosition.update('y', (y) => {
+          if (y >  (size/2)) y = y - size;
+          if (y < (size/2)*-1) y = y + size;
+          return y + action.data.y;
+        });
       });
     case BOARD:
       return state.setIn(["pieces", action.data.col, action.data.row], 1);
