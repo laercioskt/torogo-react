819c6e2c6e8167284e690797f8fe16b17655c7e1
Lucas <lucass@gmail.com>
2017-12-24 13:59:47 -0200

    Using gray rectangles as direction buttons

--- src/containers/App/board/Board.js
+++ src/containers/App/board/Board.js
@@ -22,22 +22,19 @@ render() {
     var x = this.props.boardPosition.getIn("x");
     var y = this.props.boardPosition.getIn("y");
     return (
-      <div >
-          <button onClick={() => this.props.actions.moveBoard({x: -0.36, y: 0})}>Left</button>
-          <button onClick={() => this.props.actions.moveBoard({x: 0.36, y: 0})}>Right</button>
-          <button onClick={() => this.props.actions.moveBoard({x: 0, y: -0.36})}>Up</button>
-          <button onClick={() => this.props.actions.moveBoard({x: 0, y: 0.36})}>Down</button>
-          <svg height="600" width="100%" viewBox={"-1 -1 "+(length+2)+" "+ (length+2)} preserveAspectRatio="xMinYMin meet">
-            <g transform={"translate("+x+","+y+")"} >
-              <rect height={length} width={length} y="0" x="0" fill="#ffbd13"/>
-              {svgFragments}
-            </g>
-            <g><rect height="100%" width="100" y="-1" x="-100" fill="#FF0000"/>
-            <rect height="100%" width="100" y="-1" x={length} fill="#FF0000"/>
-            <rect height="100" width="100%" y="-100" x="-1" fill="#FF0000"/>
-            <rect height="100" width="100%" y={length} x="-1" fill="#FF0000"/>
-            </g>
-          </svg>
+      <div className="noselect" >
+        <svg height="600" width="100%" viewBox={"-1 -1 "+(length+2)+" "+ (length+2)} preserveAspectRatio="xMedYMin meet">
+          <g transform={"translate("+x+","+y+")"} >
+            <rect height={length} width={length} y="0" x="0" fill="#ffbd13"/>
+            {svgFragments}
+          </g>
+          <g>
+            <rect onClick={() => this.props.actions.moveBoard({x: 0.36, y: 0})} height="100%" width="100" y="-1" x="-100" fill="#cccccc"/>
+            <rect onClick={() => this.props.actions.moveBoard({x: -0.36, y: 0})} height="100%" width="100" y="-1" x={length} fill="#cccccc"/>
+            <rect onClick={() => this.props.actions.moveBoard({x: 0, y: 0.36})} height="100" width="100%" y="-100" x="-1" fill="#cccccc"/>
+            <rect onClick={() => this.props.actions.moveBoard({x: 0, y: -0.36})} height="100" width="100%" y={length} x="-1" fill="#cccccc"/>
+          </g>
+        </svg>
       </div>
     );
   }
